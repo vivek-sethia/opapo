936be9a0d142ca87bb97fdfb7c84d8c7053f72b7
swathi-ssunder <swathi.ssunder@gmail.com>
2016-06-30 21:50:50 +0200

    [ui] - emotional meter for sentiment of reviews

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -62,7 +62,7 @@ $(document).ready(function() {
             })
         }
 
-        amazon_chart = new Highcharts.Chart({
+        new Highcharts.Chart({
             chart: {
                 renderTo: 'load',
                 margin: [0, 0, 0, 0],
@@ -146,64 +146,91 @@ $(document).ready(function() {
         new Highcharts.Chart({
             chart: {
                 renderTo: 'container',
-                type: 'scatter',
-                zoomType: 'xy'
+                type: 'gauge',
+                plotBackgroundColor: null,
+                plotBackgroundImage: null,
+                plotBorderWidth: 0,
+                plotShadow: false
             },
             title: {
-                text: 'Rating Versus Sentiment'
+                text: 'Emotional Meter'
             },
-            subtitle: {
-                text: 'Latest 30 reviews'
+            pane: {
+                startAngle: -150,
+                endAngle: 150,
+                background: [{
+                    backgroundColor: {
+                        linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                        stops: [
+                            [0, '#FFF'],
+                            [1, '#333']
+                        ]
+                    },
+                    borderWidth: 0,
+                    outerRadius: '109%'
+                }, {
+                    backgroundColor: {
+                        linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                        stops: [
+                            [0, '#333'],
+                            [1, '#FFF']
+                        ]
+                    },
+                    borderWidth: 1,
+                    outerRadius: '107%'
+                }, {
+                    // default background
+                }, {
+                    backgroundColor: '#DDD',
+                    borderWidth: 0,
+                    outerRadius: '105%',
+                    innerRadius: '103%'
+                }]
             },
-            xAxis: {
-                title: {
-                    enabled: true,
-                    text: 'Sentiment ()'
-                },
-                startOnTick: true,
-                endOnTick: true,
-                showLastLabel: true
-            },
+            // the value axis
             yAxis: {
+                min: -4,
+                max: 4,
+
+                minorTickInterval: 'auto',
+                minorTickWidth: 1,
+                minorTickLength: 10,
+                minorTickPosition: 'inside',
+                minorTickColor: '#666',
+
+                tickPixelInterval: 30,
+                tickWidth: 2,
+                tickPosition: 'inside',
+                tickLength: 10,
+                tickColor: '#666',
+                labels: {
+                    step: 2,
+                    rotation: 'auto'
+                },
                 title: {
-                    text: 'Rating (star)'
+                    text: ''
+                },
+                plotBands: [{
+                    from: -4,
+                    to: -1,
+                    color: '#DF5353' // red
+                }, {
+                    from: -1,
+                    to: 1,
+                    color: '#DDDF0D' // yellow
+                }, {
+                    from: 1,
+                    to: 4,
+                    color: '#55BF3B' // green
+                }]
+            },
+            series: [{
+                name: 'Sentiment',
+                data: [data.amazon.overall_sentiment],
+                tooltip: {
+                    valueSuffix: ''
                 }
-            },
-            legend: {
-                layout: 'vertical',
-                align: 'left',
-                verticalAlign: 'top',
-                x: 100,
-                y: 70,
-                floating: true,
-                backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF',
-                borderWidth: 1
-            },
-            plotOptions: {
-                scatter: {
-                    marker: {
-                        radius: 5,
-                        states: {
-                            hover: {
-                                enabled: true,
-                                lineColor: 'rgb(100,100,100)'
-                            }
-                        }
-                    },
-                    states: {
-                        hover: {
-                            marker: {
-                                enabled: false
-                            }
-                        }
-                    },
-                    tooltip: {
-                        headerFormat: '<b>{series.name}</b><br>'
-                        //pointFormat: '{point.x} cm, {point.y} kg'
-                    }
-                }
-            },
-            series: data.amazon.review_sentiments
+            }]
         });
     });
 });
