2314dac221b774557c90bf7a614aada1fe85d110
swathi-ssunder <swathi.ssunder@gmail.com>
2016-07-15 16:33:31 +0200

    [ui] - rating and rating stats displayed for ebay

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -17,7 +17,11 @@ $(document).ready(function() {
 
     $.getJSON("../data.json", function(data) {
 
-        var star, price_arr, ratings = [], amazon_avg_rating;
+        var star, price_arr, ratings = [], amazon_avg_rating, ebay_avg_rating,
+            rating_stats = {
+                'amazon': {'renderTo': 'amazon_rating_stats', 'data': []},
+                'ebay': {'renderTo': 'ebay_rating_stats', 'data': []}
+            };
 
         console.log(data);
 
@@ -33,12 +37,16 @@ $(document).ready(function() {
         if(isInStock(data.amazon.availability || '')) {
             $('#amazon_availability').attr('src', '../static/img/yes.png');
         }
-        $('#amazon_review_count').text(data.amazon.rating.review_count + ' reviews');
+        if(data.amazon.rating.review_count)
+            $('#amazon_review_count').text(data.amazon.rating.review_count + ' reviews');
+
         $('#amazon_product_img').attr('src', data.amazon.image);
         $('#amazon_product_link').attr('href', data.amazon.product_link);
 
-        amazon_avg_rating = (parseFloat(data.amazon.rating.average.trim('out of 5 stars'))/5)*100;
-        $('.amazon_features .star-ratings-sprite-rating').css('width', amazon_avg_rating);
+        if(data.amazon.rating.average) {
+            amazon_avg_rating = (parseFloat(data.amazon.rating.average.trim('out of 5 stars'))/5)*100;
+            $('.amazon_features .star-ratings-sprite-rating').css('width', amazon_avg_rating + '%');
+        }
 
         price_arr = (data.ebay.price).split(".");
 
@@ -50,190 +58,177 @@ $(document).ready(function() {
         if(isInStock(data.ebay.availability || '')) {
             $('#ebay_availability').attr('src', '../static/img/yes.png');
         }
-        $('#ebay_review_count').text(data.ebay.rating.review_count);
+
+        if(data.ebay.rating.review_count)
+            $('#ebay_review_count').text(data.ebay.rating.review_count + ' reviews');
+
         $('#ebay_product_img').attr('src', data.ebay.image);
         $('#ebay_product_link').attr('href', data.ebay.product_link);
 
-        /*Format the ratings as per data format of charts*/
-        $.each(data.amazon.rating.stats, function(star, rating) {
-            ratings.push({
-                name: star,
-                y: parseFloat(rating)
-            })
-        });
+        if(data.ebay.rating.average) {
+            ebay_avg_rating = (parseFloat(data.ebay.rating.average.trim('out of 5 stars'))/5)*100;
+            $('.ebay_features .star-ratings-sprite-rating').css('width', ebay_avg_rating + '%');
+        }
 
-        /*Donut chart for distribution of rating in reviews*/
-        new Highcharts.Chart({
-            chart: {
-                renderTo: 'load',
-                margin: [0, 0, 0, 0],
-                backgroundColor: null,
-                plotBackgroundColor: 'none'
-            },
-            title: {
-                text: null
-            },
-            tooltip: {
-                formatter: function() {
-                    return this.point.name + ': ' + this.y + ' %';
-                }
-            },
-            legend: {
-                enabled: true,
-                align: 'right',
-                layout: 'vertical',
-                 itemStyle: {
-                    color: 'white',
-                    font: '12pt Trebuchet MS, Verdana, sans-serif'
-                 }
-            },
-            plotOptions: {
-                pie: {
-                    showInLegend: true
-                }
-            },
-            series: [{
-                borderWidth: 2,
-                borderColor: '#F1F3EB',
-                shadow: false,
-                type: 'pie',
-                name: 'Income',
-                innerSize: '65%',
-                data: ratings,
-                dataLabels: {
-                    enabled: false,
-                    color: '#000000',
-                    connectorColor: '#000000'
-                }
-            }]
-        });
+        if(data.amazon.rating.stats) {
+            /*Format the ratings as per data format of charts*/
+            $.each(data.amazon.rating.stats, function(star, rating) {
+                rating_stats['amazon'].data.push({
+                    name: star,
+                    y: parseFloat(rating)
+                })
+            });
+        }
+        if(data.ebay.rating.stats) {
+            /*Format the ratings as per data format of charts*/
+            $.each(data.ebay.rating.stats, function(star, rating) {
+                rating_stats['ebay'].data.push({
+                    name: star,
+                    y: parseFloat(rating)
+                })
+            });
+        }
 
-        /*ebay_chart = new Highcharts.Chart({
-            chart: {
-                renderTo: 'space',
-                margin: [0, 0, 0, 0],
-                backgroundColor: null,
-                plotBackgroundColor: 'none'
-            },
-            title: {
-                text: null
-            },
-            tooltip: {
-                formatter: function() {
-                    return this.point.name + ': ' + this.y + ' %';
-                }
-            },
-            series: [{
-                borderWidth: 2,
-                borderColor: '#F1F3EB',
-                shadow: false,
-                type: 'pie',
-                name: 'SiteInfo',
-                innerSize: '65%',
-                data: [{
-                    name: 'Used',
-                    y: 65.0
-                }, {
-                    name: 'Rest',
-                    y: 35.0
-                }],
-                dataLabels: {
-                    enabled: false,
-                    connectorColor: '#000000'
-                }
-            }]
-        });*/
-
-        /*Gauge chart for overall sentiment of reviews*/
-        new Highcharts.Chart({
-            chart: {
-                renderTo: 'container',
-                type: 'gauge',
-                plotBackgroundColor: null,
-                plotBackgroundImage: null,
-                plotBorderWidth: 0,
-                plotShadow: false
-            },
-            title: {
-                text: 'Emotional Meter'
-            },
-            pane: {
-                startAngle: -150,
-                endAngle: 150,
-                background: [{
-                    backgroundColor: {
-                        linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
-                        stops: [
-                            [0, '#FFF'],
-                            [1, '#333']
-                        ]
-                    },
-                    borderWidth: 0,
-                    outerRadius: '109%'
-                }, {
-                    backgroundColor: {
-                        linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
-                        stops: [
-                            [0, '#333'],
-                            [1, '#FFF']
-                        ]
-                    },
-                    borderWidth: 1,
-                    outerRadius: '107%'
-                }, {
-                    // default background
-                }, {
-                    backgroundColor: '#DDD',
-                    borderWidth: 0,
-                    outerRadius: '105%',
-                    innerRadius: '103%'
-                }]
-            },
-            // the value axis
-            yAxis: {
-                min: -4,
-                max: 4,
-
-                minorTickInterval: 'auto',
-                minorTickWidth: 1,
-                minorTickLength: 10,
-                minorTickPosition: 'inside',
-                minorTickColor: '#666',
-
-                tickPixelInterval: 30,
-                tickWidth: 2,
-                tickPosition: 'inside',
-                tickLength: 10,
-                tickColor: '#666',
-                labels: {
-                    step: 2,
-                    rotation: 'auto'
+        $.each(rating_stats, function(index, stat) {
+            /*Donut chart for distribution of rating in reviews*/
+            new Highcharts.Chart({
+                chart: {
+                    renderTo: stat['renderTo'],
+                    margin: [0, 0, 0, 0],
+                    backgroundColor: null,
+                    plotBackgroundColor: 'none'
                 },
                 title: {
-                    text: ''
+                    text: null
                 },
-                plotBands: [{
-                    from: -4,
-                    to: -1,
-                    color: '#DF5353' // red
-                }, {
-                    from: -1,
-                    to: 1,
-                    color: '#DDDF0D' // yellow
-                }, {
-                    from: 1,
-                    to: 4,
-                    color: '#55BF3B' // green
-                }]
-            },
-            series: [{
-                name: 'Sentiment',
-                data: [data.amazon.overall_sentiment],
                 tooltip: {
-                    valueSuffix: ''
-                }
-            }]
+                    formatter: function() {
+                        return this.point.name + ': ' + this.y + ' %';
+                    }
+                },
+                legend: {
+                    enabled: true,
+                    align: 'right',
+                    layout: 'vertical',
+                     itemStyle: {
+                        color: 'white',
+                        font: '12pt Trebuchet MS, Verdana, sans-serif'
+                     }
+                },
+                plotOptions: {
+                    pie: {
+                        showInLegend: true
+                    }
+                },
+                series: [{
+                    borderWidth: 2,
+                    borderColor: '#F1F3EB',
+                    shadow: false,
+                    type: 'pie',
+                    name: 'Income',
+                    innerSize: '65%',
+                    data: stat['data'],
+                    dataLabels: {
+                        enabled: false,
+                        color: '#000000',
+                        connectorColor: '#000000'
+                    }
+                }]
+            });
         });
+
+        if(data.amazon.overall_sentiment) {
+            /*Gauge chart for overall sentiment of reviews*/
+            new Highcharts.Chart({
+                chart: {
+                    renderTo: 'container',
+                    type: 'gauge',
+                    plotBackgroundColor: null,
+                    plotBackgroundImage: null,
+                    plotBorderWidth: 0,
+                    plotShadow: false
+                },
+                title: {
+                    text: 'Emotional Meter'
+                },
+                pane: {
+                    startAngle: -150,
+                    endAngle: 150,
+                    background: [{
+                        backgroundColor: {
+                            linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                            stops: [
+                                [0, '#FFF'],
+                                [1, '#333']
+                            ]
+                        },
+                        borderWidth: 0,
+                        outerRadius: '109%'
+                    }, {
+                        backgroundColor: {
+                            linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                            stops: [
+                                [0, '#333'],
+                                [1, '#FFF']
+                            ]
+                        },
+                        borderWidth: 1,
+                        outerRadius: '107%'
+                    }, {
+                        // default background
+                    }, {
+                        backgroundColor: '#DDD',
+                        borderWidth: 0,
+                        outerRadius: '105%',
+                        innerRadius: '103%'
+                    }]
+                },
+                // the value axis
+                yAxis: {
+                    min: -4,
+                    max: 4,
+
+                    minorTickInterval: 'auto',
+                    minorTickWidth: 1,
+                    minorTickLength: 10,
+                    minorTickPosition: 'inside',
+                    minorTickColor: '#666',
+
+                    tickPixelInterval: 30,
+                    tickWidth: 2,
+                    tickPosition: 'inside',
+                    tickLength: 10,
+                    tickColor: '#666',
+                    labels: {
+                        step: 2,
+                        rotation: 'auto'
+                    },
+                    title: {
+                        text: ''
+                    },
+                    plotBands: [{
+                        from: -4,
+                        to: -1,
+                        color: '#DF5353' // red
+                    }, {
+                        from: -1,
+                        to: 1,
+                        color: '#DDDF0D' // yellow
+                    }, {
+                        from: 1,
+                        to: 4,
+                        color: '#55BF3B' // green
+                    }]
+                },
+                series: [{
+                    name: 'Sentiment',
+                    data: [data.amazon.overall_sentiment],
+                    tooltip: {
+                        valueSuffix: ''
+                    }
+                }]
+            });
+        }
 
         if(data.amazon.tones) {
             $.each(data.amazon.tones, function(toneName, tone) {
