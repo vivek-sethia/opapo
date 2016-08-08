992d2336165bb49b9d3ec28795b0022b869d2289
Shankar13 <shanx13@gmail.com>
2016-08-08 22:58:55 +0200

    Config

--- config.py
+++ config.py
@@ -2,14 +2,3 @@
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
-PUBLIC_KEY = 'AKIAIUK5I2RHU23KE6RQ'
-                
-PRIVATE_KEY = '7nw+YnqQBNNok5Gcjs+jfB1DR22ZGdAi3ApWuZjo'
-            
-ASSOCIATE_TAG = 'assoid-20'
-
-WDC_TA_USER_NAME = 'bf8feff6-5e70-4e3f-a0e6-b035705c6303'
-
-WDC_TA_PASSWORD = '6zpwoJ0ZUnTo'
-
-WDC_TA_VERSION = '2016-06-30'
